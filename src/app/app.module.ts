ad4c0162e582bc89bc176460aaac4157452dc8c4
Jaime Cadle <jaime@coficiency.com>
2018-08-03 14:39:18 -0500

    Initial commit; install Bootstrap and ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModalModule } from "@ng-bootstrap/ng-bootstrap";
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
