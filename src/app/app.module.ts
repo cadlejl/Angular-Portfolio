a7c7808ca946b068d6052b9e7ef22fff47d00d61
Jaime Cadle <jaime@coficiency.com>
2018-08-04 13:08:29 -0500

    Add app-details and app-form components, install ngx-bootstrap and import ModalModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { NgbModalModule } from "@ng-bootstrap/ng-bootstrap";
+import { ModalModule } from "ngx-bootstrap/modal";
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AppShowcaseComponent } from './app-showcase/app-showcase.component';
+import { AppDetailsComponent } from './app-details/app-details.component';
+import { AppFormComponent } from './app-form/app-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AppShowcaseComponent
+    AppShowcaseComponent,
+    AppDetailsComponent,
+    AppFormComponent
   ],
   imports: [
     BrowserModule,
-    NgbModalModule.forRoot()
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
